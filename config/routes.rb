12c5c6e2cd35ceb4fdebc91d07f7bc41c04cbb68
Pablo Arango <pabloaran17@gmail.com>
2016-11-12 16:00:04 -0800

    root route to questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :questions
-  root to: "home#index"
+
+  root to: "questions#index"
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   devise_scope :user do
     delete "/users/sign_out" => "devise/sessions#destroy"
   end
