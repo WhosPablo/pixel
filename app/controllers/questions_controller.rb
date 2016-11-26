8ea0116ca629d7ec970c57e83629a543edada5ab
Luciano <lucianoarango@college.harvard.edu>
2016-11-26 13:17:23 -0800

    fix question deletion

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -65,8 +65,9 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully deleted.' }
+      format.html { redirect_to root_path, notice: 'Question was successfully deleted.' }
       format.json { head :no_content }
+      format.js
     end
   end
 
