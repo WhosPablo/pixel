4a59f19c1af83163b45ebbf47902fc0fb3bc83b8
Luciano <lucianoarango@college.harvard.edu>
2016-11-28 22:13:51 -0800

    Clean up join to only return distinct records

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @new_question = Question.new
     #TODO change waiting on https://github.com/rails/rails/issues/24055
-    @questions = Question.left_outer_joins(:question_recipients).where('question_recipients.user_id = ? OR questions.user_id = ?',
+    @questions = Question.left_outer_joins(:question_recipients).distinct.where('question_recipients.user_id = ? OR questions.user_id = ?',
                                                                        current_user.id, current_user.id)
   end
 end
