403c1d11ac412ff65a0d5a9ea4ff96cf9d6f113f
Luciano <lucianoarango@college.harvard.edu>
2016-12-05 23:34:18 -0800

    Fix notifications order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       @fresh_activities = current_user.get_new_notifications
       if @fresh_activities.count < 5
-        @other_activities = current_user.activities.first(5)
+        @other_activities = current_user.activities.order('created_at DESC').first(5)
       end
     end
   end
