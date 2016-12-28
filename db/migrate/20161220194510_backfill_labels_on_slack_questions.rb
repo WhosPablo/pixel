b72454091ffb8c879d68af59579f9c713965a5aa
Luciano <lucianoarango@college.harvard.edu>
2016-12-27 22:21:32 -0500

    Remove migration for pre filling

--- db/migrate/20161220194510_backfill_labels_on_slack_questions.rb
+++
@@ -1,9 +0,0 @@
-class BackfillLabelsOnSlackQuestions < ActiveRecord::Migration[5.0]
-  def change
-    Question.reset_column_information
-    for question1 in Question.all
-      question1.auto_populate_labels!
-      question1.save
-    end
-  end
-end
