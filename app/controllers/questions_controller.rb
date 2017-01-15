d67ccbf799b53d8900a8c1b8e14c917fb53c17c1
Luciano <lucianoarango@college.harvard.edu>
2017-01-14 22:29:25 -0800

    restarting update issue

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :require_ownership, only: [:edit, :update, :destroy]
   before_action :check_permission, only: [:show]
-  before_action :find_notifications, only: [:show, :edit, :index ]
+  before_action :find_notifications, only: [:show, :edit, :index]
 
   # GET /questions
   # GET /questions.json
