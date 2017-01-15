f61ad6110aa3390aae3f24297837c97d2214dba4
Luciano <lucianoarango@college.harvard.edu>
2017-01-14 21:49:47 -0800

    picking up less questions

--- app/slack/quiki-slack-bot/commands/question_scraper.rb
+++ app/slack/quiki-slack-bot/commands/question_scraper.rb
@@ -1,5 +1,6 @@
 class Expressions
-  IGNORE = /(meet)|(tomorrow)|(today)|(yesterday)|(lunch)/
+  HAS = /(who)|(what)|(when)|(where)|(why)|(how)|(should)|(can)|(do)|(question)/
+  IGNORE = /(meet)|(tomorrow)|(today)|(yesterday)|(lunch)|(\")/
 end
 
 module QuikiBot
@@ -36,20 +37,22 @@ module QuikiBot
 
         unless words.keys.count == 0 or data.user.blank? or text.downcase =~ Expressions::IGNORE
 
-          team = SlackTeam.find_by_team_id(data.team)
+          if text.downcase =~ Expressions::HAS
+            team = SlackTeam.find_by_team_id(data.team)
 
-          full_client =  Slack::Web::Client.new
-          full_client.token = team.token
+            full_client =  Slack::Web::Client.new
+            full_client.token = team.token
 
-          creator = SlackQaJobHelper.find_user_by_slack_id(full_client, data.user, team.company)
+            creator = SlackQaJobHelper.find_user_by_slack_id(full_client, data.user, team.company)
 
-          new_question = SlackQaJobHelper.create_question(creator, text, team.company)
+            new_question = SlackQaJobHelper.create_question(creator, text, team.company)
 
-          message = SlackQaJobHelper.find_question_and_confirm(text, team.company, new_question)
+            message = SlackQaJobHelper.find_question_and_confirm(text, team.company, new_question)
 
-          full_client.chat_postMessage(channel: data.channel, text: message[:text], attachments: message[:attachments])
+            full_client.chat_postMessage(channel: data.channel, text: message[:text], attachments: message[:attachments])
 
-          SlackQaJobHelper.populate_question(data.user, data.team, data.channel, new_question, full_client, team.company)
+            SlackQaJobHelper.populate_question(data.user, data.team, data.channel, new_question, full_client, team.company)
+          end
         end
       end
     end
